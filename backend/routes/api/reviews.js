b0326b50e170f3f6f098ab1b577d64a056a6ca49
Omega Dziko F. <megaoy@yahoo.com>
2024-10-20 15:10:31 -0400

    add image to review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,6 +1,6 @@
 // routes/api/reviews.js
 const express = require('express');
-const { Review, User, Spot, ReviewImage } = require('../../db/models');
+const { Review, User, Spot, ReviewImage, SpotImage } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');  // Middleware for authentication
 const router = express.Router();
 
@@ -38,5 +38,54 @@ router.get('/current', requireAuth, async (req, res) => {
   // Format the response
   res.json({ Reviews: reviews });
 });
+
+// POST /reviews/:reviewId/images
+router.post('/:reviewId/images', requireAuth, async (req, res) => {
+    const { url } = req.body;
+    const reviewId = req.params.reviewId;
+    const userId = req.user.id;
+
+    try {
+
+      const review = await Review.findByPk(reviewId);
+
+      //when there is no matching review
+      if (!review) {
+
+        return res.status(404).json({ message: "No Matching Review Found" });
+      }
+
+      // make sure review and user match
+      if (review.userId !== userId) {
+
+        return res.status(403).json({ message: "This Revie Does Not Belong to You" });
+      }
+
+      // Setting max
+      const imageCount = await ReviewImage.count({ where: { reviewId } });
+
+   // Max 5 images
+      if (imageCount >= 5) {
+
+        return res.status(403).json({ message: "Maximum number of images reached" });
+      }
+
+      // add new image
+      const newImage = await ReviewImage.create({
+        reviewId,
+        url,
+      });
+
+      //  Return created image data
+      return res.status(201).json({
+        id: newImage.id,
+        url: newImage.url,
+      });
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
 
 module.exports = router;
